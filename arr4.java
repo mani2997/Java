950be1626bfa4428c3a4588809a716159675937d
mani2997 <50626920+mani2997@users.noreply.github.com>
2019-05-14 19:22:55 +0530

    Add files via upload

---
+++ arr4.java
@@ -0,0 +1,15 @@
+class Mani
+{
+	public static void main(String[]args)
+	{
+		int number=12;
+		switch(number%4)
+		{
+			case 0:
+			System.out.println("Divisible by 4");
+			break;
+			case 1:
+			System.out.println("The number is divided by 3");
+		}
+	}
+}
